d4f54f9f5b743ccf71782067747bb6789148a3f1
Paulo0264 <fagundes.dev@hotmail.com>
2024-08-05 23:08:20 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2 className="text-red-600">Home Page</h2>
+      <Button>teste</Button>
     </main>
   );
 }
